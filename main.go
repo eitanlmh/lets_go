59edb413d3390ea697bed8e24f937baf91067b7f
Eitan Gribov <eitanlmh@gmail.com>
2026-03-17 12:24:31 +0200

    created normal functions and closed channels

--- main.go
+++ main.go
@@ -5,33 +5,40 @@ import (
 	"time"
 )
 
+func numbers(numbers_channel chan int, start int, end int) {
+	for i := start; i < end; i++ {
+		time.Sleep(100 * time.Millisecond)
+		numbers_channel <- i
+	}
+	close(numbers_channel)
+}
+func letters(letters_channel chan string) {
+	letters := "abcdefghijklmnopqrstuvwxyz"
+	for _, value := range letters {
+		time.Sleep(500 * time.Millisecond)
+		letters_channel <- string(value)
+	}
+	close(letters_channel)
+}
 func main() {
 
 	num_channel := make(chan int)
 	letters_channel := make(chan string)
 
-	go func(start int, end int) {
-		for i := start; i < end; i++ {
-			time.Sleep(100 * time.Millisecond)
-			num_channel <- i
-		}
-	}(1, 10)
-
-	go func() {
-		letters := "abcdefghijklmnopqrstuvwxyz"
-		for _, value := range letters {
-			time.Sleep(500 * time.Millisecond)
-			letters_channel <- string(value)
-		}
-	}()
+	go numbers(num_channel, 1, 10)
+	go letters(letters_channel)
 
 outer:
 	for {
 		select {
-		case msg1 := <-num_channel:
-			fmt.Println("message from num channel:", msg1)
-		case msg2 := <-letters_channel:
-			fmt.Println("message from letters channel:", msg2)
+		case msg1, ok := <-num_channel:
+			if ok {
+				fmt.Println("message from num channel:", msg1)
+			}
+		case msg2, ok := <-letters_channel:
+			if ok {
+				fmt.Println("message from letters channel:", msg2)
+			}
 		case <-time.After(3 * time.Second):
 			fmt.Println("timeout")
 			break outer
