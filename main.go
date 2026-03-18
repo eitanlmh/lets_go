cab6c52c2a874098b5560555a876e6f498bf4806
Eitan Gribov <eitanlmh@gmail.com>
2026-03-18 13:12:03 +0200

    removed label

--- main.go
+++ main.go
@@ -5,14 +5,14 @@ import (
 	"time"
 )
 
-func numbers(numbers_channel chan int, start int, end int) {
+func numbers(numbers_channel chan<- int, start int, end int) {
 	for i := start; i < end; i++ {
 		time.Sleep(100 * time.Millisecond)
 		numbers_channel <- i
 	}
 	close(numbers_channel)
 }
-func letters(letters_channel chan string) {
+func letters(letters_channel chan<- string) {
 	letters := "abcdefghijklmnopqrstuvwxyz"
 	for _, value := range letters {
 		time.Sleep(500 * time.Millisecond)
@@ -28,22 +28,23 @@ func main() {
 	go numbers(num_channel, 1, 10)
 	go letters(letters_channel)
 
-outer:
 	for {
 		select {
-		case msg1, ok := <-num_channel:
-			if ok {
+		case msg1, num_ok := <-num_channel:
+			if num_ok {
 				fmt.Println("message from num channel:", msg1)
+			} else{
+				num_channel = nil
 			}
-		case msg2, ok := <-letters_channel:
-			if ok {
+		case msg2, letter_ok := <-letters_channel:
+			if letter_ok {
 				fmt.Println("message from letters channel:", msg2)
+			} else {
+				letters_channel = nil
 			}
 		case <-time.After(3 * time.Second):
-			fmt.Println("timeout")
-			break outer
+				fmt.Println("timeout")
+				return
 		}
-
 	}
-
 }
